31c488e52188f22b6accd957b88a8fcfbdf0f3c3
Dmitry Matul <matuldmtr@gmail.com>
2024-01-17 21:26:00 +0100

    feat: isContainNumber complete

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -268,8 +268,14 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const strFromNumb = `${num}`;
+  const strFromDigit = String(digit);
+
+  for (let i = 0; i < strFromNumb.length; i += 1) {
+    if (strFromNumb[i] === strFromDigit) return true;
+  }
+  return false;
 }
 
 /**
